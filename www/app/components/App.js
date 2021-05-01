ca4276ae43611e8b66f00a4799cc853fddf36df0
developerlin123 <developerlin123@gmail.com>
2021-05-01 22:18:54 +0200

    still have problem with fucking dom rendering

--- www/app/components/App.js
+++ www/app/components/App.js
@@ -2,22 +2,52 @@ import React from 'react';
 import Yinyue from "./yinyue/Yinyue.js";
 import Xinwen from "./xinwen/Xinwen.js";
 import Home from "./home/Home.js";
-import { Route , Link } from 'react-router-dom'
+import { Route , Link , NavLink } from 'react-router-dom';
 
+
+let testactive=(hash)=>{
+	var hashArr=window.location.hash.substr(2).split("/");
+	console.log(hashArr)
+	if(hashArr[0]==hash){
+       return "active"
+	}
+	return "";
+}
 
 export default () => {
 	return (
 		<section>
-			<header>
-				<Link to="/">首页</Link>
-				<Link to="/yinyue">音乐</Link>
-				<Link to="/xinwen">新闻</Link>
-			</header>
+			<nav className="navbar navbar-default">
+				<div className="container-fluid">
+					<div className="navbar-header">
+					<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span className="sr-only">Toggle navigation</span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+						<span className="icon-bar"></span>
+					</button>
+					<a className="navbar-brand" href="#">P2P_InvestmentPlatform</a>
+					</div>
+
+					<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul className="nav navbar-nav">
+							<li className={testactive("")}><Link to="/">Home<span className="sr-only"></span></Link></li>
+							<li className={testactive("invest")}><Link to="/invest">invest<span className="sr-only"></span></Link></li>
+							{/* <li className={testactive("")}><a href="/">Home<span className="sr-only"></span></a></li>
+							<li className={testactive("invest")}><a href="#/invest">invest<span className="sr-only"></span></a></li> */}
+						</ul>
+						<ul className="nav navbar-nav navbar-right">
+							<li><a href="#">welcome</a></li>
+						</ul>
+					</div>
+				   </div>
+			    </nav>
 			<section className="main">
 				<Route exact path="/" component={Home}></Route>
 				<Route path="/yinyue" component={Yinyue}></Route>
 				<Route path="/xinwen" component={Xinwen}></Route>
 			</section>
+
 		</section>
 	);
 }
--- www/app/main.js
+++ www/app/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { combineReducers , createStore } from 'redux';
+import { combineReducers , applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { Route , Link , HashRouter as Router , browserHistory } from 'react-router-dom'
 import { routerReducer , syncHistoryWithStore } from 'react-router-redux';
@@ -9,16 +9,22 @@ import {createHashHistory} from 'history'
 import reducer from './reducers/index.js';
 import App from './components/App.js';
 
+import thunk from "redux-thunk";
+import { createLogger } from 'redux-logger'
+
+
 const store = createStore(
   combineReducers({
     reducer,
     routing: routerReducer
-  })
+  }),
+  applyMiddleware(createLogger(),thunk)
 );
 
 render(
+	// <Provider store={store} history={createHistory()}>
 	<Provider store={store} history={createHashHistory()}>
-	  <Router hashType="hashbang" >
+	  <Router>
 	  	<App></App>
 	  </Router>
 	</Provider>
